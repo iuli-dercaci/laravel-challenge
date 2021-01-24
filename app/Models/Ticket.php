f6b8f94884ceb397b5ca8a5124a836749ae2ab56
Iuli Dercaci <iuli.dercaci@maglabs.net>
2021-01-24 21:51:56 +0000

    Add fillable fields for a `Ticket` model, added strict mode

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -1,15 +1,23 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
     use HasFactory;
 
-    public function user()
+    public $fillable = [
+        'subject',
+        'content',
+        'status',
+    ];
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
