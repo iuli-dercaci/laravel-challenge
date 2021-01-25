5c248e0924cd39566558e222579e104562453eb3
Iuli Dercaci <iuli.dercaci@maglabs.net>
2021-01-25 11:56:26 +0000

    Implement open tickets API payload, add corresponding tests

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,13 +3,15 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    public function openTickets(): void
+    public function openTickets(Request $request)
     {
-        abort(200);
+        $perPage = $request->get('per_page') ?: 10;
+        return Ticket::open()->paginate($perPage);
     }
 
     public function closedTickets(): void
--- tests/Feature/Controllers/TicketControllerTest.php
+++ tests/Feature/Controllers/TicketControllerTest.php
@@ -3,14 +3,26 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Ticket;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class TicketControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public $seed = true;
+
     public function testOpenTickets(): void
     {
         $response = $this->get('/open-tickets');
-        $response->assertStatus(200);
+        $response->assertSuccessful();
+        $response->assertHeader('Content-Type', 'application/json');
+
+        $total = Ticket::open()->count();
+        $payload = json_decode($response->getContent());
+
+        $this->assertEquals($payload->total, $total);
     }
 
     public function testClosedTickets(): void
